d5fb7a36fea0999c2d7717e1d262e9477c12d155
yooj <114331071+yooj1202@users.noreply.github.com>
2022-11-12 19:48:25 +0900

    feat: Init XCode

---
+++ withsuna/withsuna/withsunaApp.swift
@@ -0,0 +1,17 @@
+//
+//  withsunaApp.swift
+//  withsuna
+//
+//  Created by Yooj on 2022/11/12.
+//
+
+import SwiftUI
+
+@main
+struct withsunaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
